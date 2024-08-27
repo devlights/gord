0573866802e9d190b26511a523abce2b31237667
devlights <gsf.zero1@devlights.onmicrosoft.com>
2024-08-27 14:25:46 +0900

    Add examples/page_range

--- document.go
+++ document.go
@@ -32,84 +32,84 @@ func NewDocument(docs *Documents, doc *ole.IDispatch) (*Document, ReleaseFunc) {
 	return d, r
 }
 
-func (w *Document) ComObject() *ole.IDispatch {
-	return w.comObj
+func (d *Document) ComObject() *ole.IDispatch {
+	return d.comObj
 }
 
-func (w *Document) Gord() *Gord {
-	return w.gordObj.Gord()
+func (d *Document) Gord() *Gord {
+	return d.gordObj.Gord()
 }
 
-func (w *Document) Releaser() *Releaser {
-	return w.Gord().Releaser()
+func (d *Document) Releaser() *Releaser {
+	return d.Gord().Releaser()
 }
 
-func (w *Document) AllRange() (*Range, error) {
-	result, err := oleutil.CallMethod(w.ComObject(), "Range")
+func (d *Document) AllRange() (*Range, error) {
+	result, err := oleutil.CallMethod(d.ComObject(), "Range")
 	if err != nil {
 		return nil, err
 	}
 
-	r := NewRange(w, result.ToIDispatch())
+	r := NewRange(d, result.ToIDispatch())
 	return r, nil
 }
 
-func (w *Document) Range(start, end int32) (*Range, error) {
-	result, err := oleutil.CallMethod(w.ComObject(), "Range", start, end)
+func (d *Document) Range(start, end int32) (*Range, error) {
+	result, err := oleutil.CallMethod(d.ComObject(), "Range", start, end)
 	if err != nil {
 		return nil, err
 	}
 
-	r := NewRange(w, result.ToIDispatch())
+	r := NewRange(d, result.ToIDispatch())
 	return r, nil
 }
 
-func (w *Document) Characters() (*Characters, error) {
-	result, err := oleutil.GetProperty(w.ComObject(), "Characters")
+func (d *Document) Characters() (*Characters, error) {
+	result, err := oleutil.GetProperty(d.ComObject(), "Characters")
 	if err != nil {
 		return nil, err
 	}
 
-	c := NewCharacters(w, result.ToIDispatch())
+	c := NewCharacters(d, result.ToIDispatch())
 	return c, nil
 }
 
-func (w *Document) Save() error {
-	_, err := oleutil.CallMethod(w.ComObject(), "Save")
+func (d *Document) Save() error {
+	_, err := oleutil.CallMethod(d.ComObject(), "Save")
 	return err
 }
 
-func (w *Document) SaveAs2(filePath string, format constants.WdSaveFormat) error {
-	return w.SaveAsWithFileFormat(filePath, format)
+func (d *Document) SaveAs2(filePath string, format constants.WdSaveFormat) error {
+	return d.SaveAsWithFileFormat(filePath, format)
 }
 
-func (w *Document) SaveAsWithFileFormat(filePath string, format constants.WdSaveFormat) error {
-	_, err := oleutil.CallMethod(w.ComObject(), "SaveAs2", filePath, int(format))
+func (d *Document) SaveAsWithFileFormat(filePath string, format constants.WdSaveFormat) error {
+	_, err := oleutil.CallMethod(d.ComObject(), "SaveAs2", filePath, int(format))
 	return err
 }
 
-func (w *Document) SaveAsWithFileFormatDefault(filePath string) error {
-	_, err := oleutil.CallMethod(w.ComObject(), "SaveAs2", filePath, int(constants.WdSaveFormatDocumentDefault))
+func (d *Document) SaveAsWithFileFormatDefault(filePath string) error {
+	_, err := oleutil.CallMethod(d.ComObject(), "SaveAs2", filePath, int(constants.WdSaveFormatDocumentDefault))
 	return err
 }
 
-func (w *Document) SetSaved(value bool) error {
-	_, err := oleutil.PutProperty(w.ComObject(), "Saved", value)
+func (d *Document) SetSaved(value bool) error {
+	_, err := oleutil.PutProperty(d.ComObject(), "Saved", value)
 	return err
 }
 
-func (w *Document) Close() error {
-	_, err := oleutil.CallMethod(w.ComObject(), "Close", false)
+func (d *Document) Close() error {
+	_, err := oleutil.CallMethod(d.ComObject(), "Close", false)
 	return err
 }
 
-func (w *Document) PrintOut() error {
-	_, err := oleutil.CallMethod(w.ComObject(), "PrintOut", nil)
+func (d *Document) PrintOut() error {
+	_, err := oleutil.CallMethod(d.ComObject(), "PrintOut", nil)
 	return err
 }
 
 //goland:noinspection GoSnakeCaseUsage,GoBoolExpressions
-func (w *Document) ExportAsFixedFormat(path string, fmtType constants.WdExportFormat) error {
+func (d *Document) ExportAsFixedFormat(path string, fmtType constants.WdExportFormat) error {
 	var (
 		outputFileName     = path                                                         // PDF ファイルまたは XPS ファイルのパスとファイル名
 		exportFormat       = int(fmtType)                                                 // PDF 形式または XPS 形式 (WdExportFormat)
@@ -127,7 +127,7 @@ func (w *Document) ExportAsFixedFormat(path string, fmtType constants.WdExportFo
 		useISO19005_1      = false                                                        // ISO 19005-1 として標準化された PDF サブセットに PDF の使用を制限するかどうかを指定
 	)
 	_, err := oleutil.CallMethod(
-		w.ComObject(),
+		d.ComObject(),
 		"ExportAsFixedFormat",
 		outputFileName,
 		exportFormat,
@@ -145,4 +145,88 @@ func (w *Document) ExportAsFixedFormat(path string, fmtType constants.WdExportFo
 		useISO19005_1)
 
 	return err
+}
+
+func (d *Document) GetTotalPages() (int32, error) {
+	ra, err := d.AllRange()
+	if err != nil {
+		return 0, err
+	}
+
+	v, err := ra.Information(constants.WdInformationNumberOfPagesInDocument)
+	if err != nil {
+		return 0, err
+	}
+
+	return v.(int32), nil
+}
+
+func (d *Document) GoTo(what constants.WdGoToItem, which constants.WdGoToDirection, count int32) (*Range, error) {
+	result, err := oleutil.CallMethod(d.ComObject(), "GoTo", int32(what), int32(which), count)
+	if err != nil {
+		return nil, err
+	}
+
+	ra := NewRange(d, result.ToIDispatch())
+	return ra, nil
+}
+
+func (d *Document) GetPageRange(page int32) (*Range, error) {
+	var (
+		allRange   *Range
+		startRange *Range
+		endRange   *Range
+		result     *Range
+
+		startPos   int32
+		endPos     int32
+		totalPages int32
+
+		err error
+	)
+
+	startRange, err = d.GoTo(constants.WdGoToItemGoToPage, constants.WdGoToDirectionGoToAbsolute, page)
+	if err != nil {
+		return nil, err
+	}
+
+	startPos, err = startRange.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	totalPages, err = d.GetTotalPages()
+	if err != nil {
+		return nil, err
+	}
+
+	if page < totalPages {
+		endRange, err = d.GoTo(constants.WdGoToItemGoToPage, constants.WdGoToDirectionGoToAbsolute, page+1)
+		if err != nil {
+			return nil, err
+		}
+
+		endPos, err = endRange.End()
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		allRange, err = d.AllRange()
+		if err != nil {
+			return nil, err
+		}
+
+		endPos, err = allRange.End()
+		if err != nil {
+			return nil, err
+		}
+		endPos -= 1
+	}
+
+	result, err = d.Range(startPos, endPos)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
